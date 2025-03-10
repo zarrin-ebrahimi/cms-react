43176304a462967c391bb47e77683cbdd7f95ca9
zarrinebrahimi22 <zarrinebrahimi22@gmail.com>
2025-03-10 10:14:48 -0700

    create routes ang pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
-
+import routes from './routes' 
+import { useRoutes } from 'react-router-dom'
 function App() {
- // new Change
+ let router = useRoutes(routes)
  return (
     <>
-
+       {router}
     </>
   )
 }
