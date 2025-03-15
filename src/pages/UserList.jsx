91f6949ae663cd33d57c5fc86d518d1d4341dca7
zarrinebrahimi22 <zarrinebrahimi22@gmail.com>
2025-03-15 04:42:45 -0700

    Added Users List Component

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import {useState} from 'react'
+import {usersRows} from './dataUsers'
+import * as Icon from '@mui/icons-material'
 
 export default function UserList() {
+  const [userDatas, setUserDatas] = useState(usersRows)
+  const userDelete = userID =>{
+    setUserDatas(userDatas.filter(user => user.id != userID))
+  }
   return (
-    <div>UserList</div>
+    <div className='bg-slate-200 mt-3 rounded-md shadow-md p-3'>
+    <span className='text-xl '>Users List</span>
+    <ul className=' w-full mt-3 flex  flex-col  gap-3 '>
+       {userDatas.map(user =>(
+            <li key={user.id} className='grid grid-cols-6 items-center  bg-white rounded-sm shadow-md p-3'>
+            <div  className='flex gap-x-3 items-center col-span-2'>
+                <img src={user.img} alt="" className='w-8'/>
+                <span className='font-bold'>{user.username}</span> 
+            </div>
+            <span className=''>{user.email}</span>
+            <span>{user.status}</span>
+            <span>${user.transaction}</span>
+             <div className='gap-3 flex '>
+             <button className='bg-gray-300 rounded-md' onClick={()=>userDelete(user.id)}>
+                <Icon.HighlightOff className='m-1 text-red-500' fontSize='small'/>
+            </button>
+            <button className='bg-gray-300 rounded-md'>
+                <Icon.BorderColor className='m-1  text-blue-700' fontSize='small'/>
+            </button>
+            </div>
+        </li>
+       ))}
+    </ul>
+</div>
   )
 }
---
+++ src/pages/dataUsers.jsx
@@ -0,0 +1,12 @@
+const usersRows = [
+    {id:1, username:'Kamand Farahman', email:'user@gmail.com', transaction:1300, status: 'Declined', img:'images/icons8-user-32.png'},
+    {id:2, username:'Ali Ebrahimi', email:'user@gmail.com', transaction:12300, status: 'Pending', img:'images/icons8-user-32.png'},
+    {id:3, username:'Mohamad Mohamadi', email:'user@gmail.com', transaction:123, status: 'Declined', img:'images/icons8-user-32.png'},
+    {id:4, username:'Arezo Rad', email:'user@gmail.com', transaction:2220, status: 'Aprroved', img:'images/icons8-user-32.png'},
+    {id:5, username:'Amir Farahman', email:'user@gmail.com', transaction:53000, status: 'Pending', img:'images/icons8-user-32.png'},
+    {id:6, username:'Amin Farahman', email:'user@gmail.com', transaction:123, status: 'Declined', img:'images/icons8-user-32.png'},
+    {id:7, username:'Horosh Farahman', email:'user@gmail.com', transaction:9800, status: 'Aprroved', img:'images/icons8-user-32.png'},
+    {id:8, username:'Hony Farahman', email:'user@gmail.com', transaction:555000, status: 'Declined', img:'images/icons8-user-32.png'},
+] 
+
+export {usersRows}
